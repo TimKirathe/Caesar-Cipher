5942d4b7ce393888571b6f64539f3c4123f00c9e
timothykirathe <timothy.kirathe@student.moringaschool.com>
2021-12-03 19:57:35 +0300

    Created my test file, class file, build.gradle file and app.java file

---
+++ src/main/java/models/Cipher.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Cipher {
+
+    String message = "";
+    public int encrypt() {
+        return 0;
+    }
+}
---
+++ src/test/java/models/CipherTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CipherTest {
+
+    @Test
+    public void encrypt_returnsIntegerForShiftValue_Integer() {
+        Cipher newCipher = new Cipher();
+        int shift = 15;
+        assertEquals(shift, newCipher.encrypt());
+    }
+
+}
