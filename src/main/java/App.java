6bb667013c9e75ce40a763ab24c0bcffdfb661fa
timothykirathe <timothy.kirathe@student.moringaschool.com>
2021-12-04 13:54:34 +0300

    Confirmed that the .encrypt function is working and does return the encrypted String to a variable in the App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,16 @@ import models.Cipher;
 import java.util.Scanner;
 
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Cipher newCipher = new Cipher();
 
+        System.out.println("Welcome to Caesar Cipher.");
+        System.out.println("Please enter the text you would like to be ciphered.");
+
+        String userText = input.nextLine();
+
+        String cipheredText = newCipher.encrypt(userText);
+        System.out.println(cipheredText);
+    }
 }
